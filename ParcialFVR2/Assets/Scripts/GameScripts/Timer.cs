da56bc8c9fd2336b2848c7c0edc2c4d7c6553b32
jesusalba15 <jesus.albarracin@uao.edu.co>
2026-04-24 12:39:00 -0500

    Actualización

---
+++ ParcialFVR2/Assets/Scripts/GameScripts/Timer.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class TimerManager : MonoBehaviour
+{
+    public TextMeshProUGUI textoTimer;
+    public float tiempoMax = 60f;
+
+    private float tiempoActual;
+    private bool corriendo = false;
+
+    void Start()
+    {
+        tiempoActual = tiempoMax;
+        ActualizarTexto();
+    }
+
+    void Update()
+    {
+        if (corriendo)
+        {
+            tiempoActual -= Time.deltaTime;
+
+            if (tiempoActual <= 0)
+            {
+                tiempoActual = 0;
+                corriendo = false;
+                GuardarTiempo();
+            }
+
+            ActualizarTexto();
+        }
+    }
+
+    void ActualizarTexto()
+    {
+        int minutos = Mathf.FloorToInt(tiempoActual / 60);
+        int segundos = Mathf.FloorToInt(tiempoActual % 60);
+
+        textoTimer.text = string.Format("{0:00}:{1:00}", minutos, segundos);
+    }
+
+    public void IniciarTimer()
+    {
+        corriendo = true;
+    }
+
+    public void DetenerYGuardar()
+    {
+        corriendo = false;
+        GuardarTiempo();
+    }
+
+    void GuardarTiempo()
+    {
+        // 🔥 Guardamos el tiempo restante
+        PlayerPrefs.SetFloat("TiempoRestante", tiempoActual);
+        PlayerPrefs.Save();
+    }
+}
--- ParcialFVR2/Assets/Scripts/MenuScripts/FinalUI.cs
+++ ParcialFVR2/Assets/Scripts/MenuScripts/FinalUI.cs
@@ -4,10 +4,20 @@ using TMPro;
 public class FinalUI : MonoBehaviour
 {
     public TextMeshProUGUI textoNombre;
+    public TextMeshProUGUI textoTiempo;
 
     void Start()
     {
+        // Nombre
         string nombre = PlayerPrefs.GetString("PlayerName", "PILOTO");
         textoNombre.text = nombre;
+
+        // Tiempo
+        float tiempo = PlayerPrefs.GetFloat("TiempoRestante", 0f);
+
+        int minutos = Mathf.FloorToInt(tiempo / 60);
+        int segundos = Mathf.FloorToInt(tiempo % 60);
+
+        textoTiempo.text = string.Format("{0:00}:{1:00}", minutos, segundos);
     }
 }
