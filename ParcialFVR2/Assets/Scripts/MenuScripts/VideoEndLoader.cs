4e2d0ab975e0771d2d2de3ff01c42c5b0e5907d8
jesusalba15 <jesus.albarracin@uao.edu.co>
2026-04-24 22:38:54 -0500

    Actualización

---
+++ ParcialFVR2/Assets/Scripts/MenuScripts/VideoEndLoader.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoEndLoader : MonoBehaviour
+{
+    public string siguienteEscena = "SceneEpilogo";
+    private VideoPlayer videoPlayer;
+
+    void Start()
+    {
+        videoPlayer = GetComponent<VideoPlayer>();
+
+        // 🔥 Evento cuando termina el video
+        videoPlayer.loopPointReached += OnVideoEnd;
+    }
+
+    void OnVideoEnd(VideoPlayer vp)
+    {
+        SceneController.instance.LoadScene(siguienteEscena);
+    }
+}
